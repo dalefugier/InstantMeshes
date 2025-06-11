1dcc990f18d8a531c4f4849a5b16cbe47de68786
Dale <dale@mcneel.com>
2025-06-11 16:19:23 -0700

    updated AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -1,15 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("InstantMeshes")]
-[assembly: AssemblyDescription("Field-aligned mesh generator for Grasshopper®")]
-[assembly: AssemblyConfiguration("Grasshopper")]
-[assembly: AssemblyCompany("Robert McNeel & Associates")]
-[assembly: AssemblyProduct("InstantMeshes")]
-[assembly: AssemblyCopyright("Copyright © 2019, Robert McNeel & Associates. All rights reserved.")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: AssemblyInformationalVersion("2")]
 [assembly: ComVisible(false)]
 [assembly: Guid("416e37e9-85bf-439c-992f-6bac1ab72ebe")]
-[assembly: AssemblyVersion("1.0.0.0")]
